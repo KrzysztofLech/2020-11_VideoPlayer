04e9486efd7d920c8d83bba201d0892f0a51bf90
Krzysztof Lech <krzysztof.lech@24abc.pl>
2020-11-21 10:07:41 +0100

    Initial Xcode project

---
+++ VideoPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by KL on 21/11/2020.
+//  Copyright © 2020 KL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
